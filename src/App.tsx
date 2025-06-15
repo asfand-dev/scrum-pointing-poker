c638c26bebd498cb31e0dceda8db1972536f8805
asfand-dev <asfand.germany@gmail.com>
2025-06-16 00:19:36 +0200

    Change Router to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Session from "./pages/Session";
 import NotFound from "./pages/NotFound";
@@ -17,14 +17,14 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter basename={import.meta.env.VITE_BASE_URL}>
+        <HashRouter basename={import.meta.env.VITE_BASE_URL}>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/session/:sessionId" element={<Session />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </NextThemesProvider>
