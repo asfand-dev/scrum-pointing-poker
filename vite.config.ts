71e95533c442f0fa91925c77593dd4a8c0de3e72
asfand-dev <asfand.germany@gmail.com>
2025-06-15 23:45:56 +0200

    Update the Vite config file to support Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
+  base: process.env.NODE_ENV === 'production' ? '/scrum-pointing-poker/' : '/',
   server: {
     host: "::",
     port: 8080,
